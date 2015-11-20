6d6ec3d0d44c082852d7d1014fe453ddd2c790ac
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2015-11-20 19:11:55 -0200

    remove examples folder, because its only for gh-pages branch

--- examples/app.js
+++
@@ -1,9 +0,0 @@
-/*global angular*/
-
-(function () {
-  'use strict';
-
-  var demoApp = angular.module('demo', ['content-editable'])
-  .controller('Ctrl', ['$scope', function($scope) {
-  }])
-}());
