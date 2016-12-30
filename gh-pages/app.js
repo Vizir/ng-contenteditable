3e3cf970e43cb5f1bb62a8b1c266b89cab27eb3a
Fabrício Ferrari de Campos <fabricio@vizir.com.br>
2016-12-30 17:38:17 -0200

    Add gh pages

---
+++ gh-pages/app.js
@@ -0,0 +1,9 @@
+/*global angular*/
+
+(function () {
+  'use strict';
+
+  var demoApp = angular.module('demo', ['content-editable'])
+  .controller('Ctrl', ['$scope', function($scope) {
+  }])
+}());
